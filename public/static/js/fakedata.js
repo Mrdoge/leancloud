3b86848f887d30548cec2a94839f7a5f822aa2e5
Mrdoge <1647487387@qq.com>
2017-10-11 17:27:26 +0800

    1、解决文章页滚到底部出发首页加载更多BUG； 2、添加返回顶部组件；

---
+++ public/static/js/fakedata.js
@@ -0,0 +1,155 @@
+/**
+ *
+ * @authors Your Name (you@example.org)
+ * @date    2017-08-25 15:21:03
+ * @version $Id$
+ */
+
+//导航列表
+D['nav'] = [
+	{
+		name:"需求匹配",
+		link:"hello",
+		i:"&#xe608;",
+		// subnav:[
+		// 	{
+		// 		name:"碳氢化合物",
+		// 		link:"hello"
+		// 	},
+		// 	{
+		// 		name:"现货银锭",
+		// 		link:"mdcdata"
+		// 	},
+		// 	{
+		// 		name:"再生铜",
+		// 		link:"copper.html"
+		// 	}
+		// ]
+	},
+	{
+		name:"任务管理",
+		link:"hello",
+		i:"&#xe607;"
+	},
+	{
+		name:"财务管理",
+		link:"hello",
+		i:"&#xe60a;"
+	},
+	{
+		name:"个人中心",
+		link:"hello",
+		i:"&#xe635;"
+	},
+	{
+		name:"合作案例",
+		link:"hello",
+		i:"&#xe636;"
+	},
+	{
+		name:"二级菜单",
+		link:"",
+		i:"&#xe636;",
+		subnav:[
+			{
+				name:"二级菜单",
+				link:"hello"
+			},
+			{
+				name:"二级菜单",
+				link:"mdcdata"
+			},
+			{
+				name:"二级菜单",
+				link:"copper.html"
+			}
+		]
+	},
+]
+
+//用户信息
+D['user'] = {
+	is_login:true,
+	avatar:"http://otdna08z0.bkt.clouddn.com/avatar/597700302e13d.jpg?imageView2/1/w/75/h/75",
+	nickname:"刘鸡鸡",
+	available:"5352892.880000003"
+}
+
+//轮播图
+D['banner'] = [
+	{
+		name:"",
+		url:"http://otdna08z0.bkt.clouddn.com/default/20170721/5971c2c8c53ac.jpg?imageView2/1/w/800/h/380",
+	},
+	{
+		name:"",
+		url:"http://otdna08z0.bkt.clouddn.com/default/20170721/5971c302dbc4f.jpg?imageView2/1/w/800/h/380",
+	},
+	{
+		name:"",
+		url:"http://otdna08z0.bkt.clouddn.com/default/20170721/5971c324addad.png?imageView2/1/w/800/h/380",
+	},
+	{
+		name:"",
+		url:"http://otdna08z0.bkt.clouddn.com/default/20170721/5971c31a08e92.jpg?imageView2/1/w/800/h/380",
+	},
+]
+
+
+D['list'] = [
+	{
+		title:"赵薇青涩学生照",
+		href:"javascript:;",
+		disc:"一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述"
+	},
+	{
+		title:"赵薇青涩学生照",
+		href:"javascript:;",
+		disc:"一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述"
+	},
+	{
+		title:"赵薇青涩学生照",
+		href:"javascript:;",
+		disc:"一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述"
+	},
+	{
+		title:"赵薇青涩学生照",
+		href:"javascript:;",
+		disc:"一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述"
+	},
+	{
+		title:"赵薇青涩学生照",
+		href:"javascript:;",
+		disc:"一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述"
+	},
+	{
+		title:"赵薇青涩学生照",
+		href:"javascript:;",
+		disc:"一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述"
+	},
+	{
+		title:"赵薇青涩学生照",
+		href:"javascript:;",
+		disc:"一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述"
+	},
+	{
+		title:"赵薇青涩学生照",
+		href:"javascript:;",
+		disc:"一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述"
+	},
+	{
+		title:"赵薇青涩学生照",
+		href:"javascript:;",
+		disc:"一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述"
+	},
+	{
+		title:"赵薇青涩学生照",
+		href:"javascript:;",
+		disc:"一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述"
+	},
+	{
+		title:"赵薇青涩学生照",
+		href:"javascript:;",
+		disc:"一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述一些描述"
+	},
+]
